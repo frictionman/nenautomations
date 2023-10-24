8aa115a9a10f7c6107b6d52bab6acbe3859e8eff
frictionman <48338507+frictionman@users.noreply.github.com>
2023-10-23 20:38:23 -0400

    apikey.py

---
+++ apikey.py
@@ -0,0 +1,7 @@
+# apikey.py
+
+# OpenAI API Key
+OPENAI_API_KEY = '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'
+
+# Financial Modeling Prep API Key
+FMP_API_KEY = 'hob61nBqwppw3jqzGPXHmG2Ta0aAQf94'
